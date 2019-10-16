37e64fddaaf5716dce89e014fd5e2e9f8480968e
Todd Lee <toddtlee48@gmail.com>
2019-10-16 00:00:15 -0400

    Styling

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './homepage.styles.scss';
+
+import '../../components/directory/directory.component';
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => (
+    <div className='homepage'>
+        <Directory />
+    </div>
+);
+
+export default HomePage;
+
